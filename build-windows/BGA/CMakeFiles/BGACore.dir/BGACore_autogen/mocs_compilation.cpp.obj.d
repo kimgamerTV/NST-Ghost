ffd57521de0efeb63bfaa7d845cb5eb8271d0ae7
crypticday <121768693+Chanomhub@users.noreply.github.com>
2025-12-12 00:41:43 +0700

    [RM] buiild windows

--- build-windows/BGA/CMakeFiles/BGACore.dir/BGACore_autogen/mocs_compilation.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-BGA/CMakeFiles/BGACore.dir/BGACore_autogen/mocs_compilation.cpp.obj: \
- /home/jop/work/NST/NST/build-windows/BGA/BGACore_autogen/mocs_compilation.cpp
--- build-windows/CMakeFiles/NST.dir/NST_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/NST.dir/NST_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- /home/jop/work/NST/NST/build-windows/NST_autogen/EWIEGA46WW/qrc_resources.cpp
