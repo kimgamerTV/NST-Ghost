433f5d10f00e4b602ff3970ffd6f93d454514d4e
crypticday <121768693+Chanomhub@users.noreply.github.com>
2026-01-07 02:20:52 +0700

    chore: update .gitignore to exclude CMake artifacts

--- CMakeFiles/NST.dir/NST_autogen/UVLADIE3JM/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/NST.dir/NST_autogen/UVLADIE3JM/qrc_resources.cpp.o: \
- /home/jop/work/NST/NST/NST_autogen/UVLADIE3JM/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/NST.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for NST.
-# This may be replaced when dependencies are built.
